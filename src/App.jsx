f3277a7cb8609e7346f7a72c74d5a3bd57b205bd
loedroptaxi-del <leodroptaxi@gmail.com>
2025-12-16 14:42:09 +0530

    jery

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
               <AddBlogs/>
           }
         />
-        <Route path="/blog"
+        <Route path="/"
           element={
               <AddDepartmentModal/>
           }
